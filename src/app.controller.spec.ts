852b94971ed0506c3712235c40ed49a7f4923960
20015223@hq.corp.leroymerlin.com <aboubakarsiriki60@gmail.com>
2025-01-31 21:49:45 +0100

    feat: config mongo

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { mongoConfig } from './config/mongo.config';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(mongoConfig.mongoConfig(), {
+          dbName: mongoConfig.database(),
+        }),
+      ],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -15,8 +24,9 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return MongoDB connection status', async () => {
+      const result = await appController.getHello();
+      expect(result).toContain('Connexion à MongoDB');
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { mongoConfig } from './config/mongo.config';
 
 @Module({
-  imports: [],
+  imports: [
+    NestConfigModule.forRoot(),
+    ConfigModule,
+    MongooseModule.forRoot(mongoConfig.mongoConfig(), {
+      dbName: mongoConfig.database(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
