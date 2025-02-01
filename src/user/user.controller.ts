dc386da80aeaf7f3dd2f60c5ceff95a05fe72bd7
20015223@hq.corp.leroymerlin.com <aboubakarsiriki60@gmail.com>
2025-02-01 17:51:19 +0100

    feat: env

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,7 +8,8 @@ import {
   Delete,
   UsePipes,
   ValidationPipe,
-  UseGuards, Query,
+  UseGuards,
+  Query,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
