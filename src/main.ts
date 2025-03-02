b46716521f944dc0060897bbe1502113031cc5d7
20015223@hq.corp.leroymerlin.com <aboubakarsiriki60@gmail.com>
2025-03-02 22:42:07 +0100

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.URL_FRONT, // 🔹 Autorise uniquement ton frontend
+    origin: 'http://localhost:5482', // 🔹 Autorise uniquement ton frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // 🔥 Permet d'envoyer les cookies et headers sécurisés
   });
