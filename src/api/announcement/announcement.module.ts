7dbf2198ef1c0b823f85e84d9e98751e1274ab74
aboubakar DIAKITE <aboubakarsiriki60@gmail.com>
2025-07-07 09:26:23 +0200

    fix: remove user

--- src/api/announcement/announcement.module.ts
+++ src/api/announcement/announcement.module.ts
@@ -7,13 +7,15 @@ import { UserModule } from '../user/user.module';
 import { FavoritesAnnouncementModule } from '../favorites-announcement/favorites-announcement.module';
 import { UserService } from '../../common/services/user/user.service';
 import { VolunteerModule } from '../volunteer/volunteer.module';
+import { AssociationModule } from '../association/association.module';
 
 @Module({
   imports: [
     DatabaseModule,
     UserModule,
-    forwardRef(() => FavoritesAnnouncementModule),
     forwardRef(() => VolunteerModule),
+    forwardRef(() => FavoritesAnnouncementModule),
+    forwardRef(() => AssociationModule),
   ],
   providers: [AnnouncementRepository, AnnouncementService, UserService],
   controllers: [AnnouncementController],
--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -5,9 +5,15 @@ import { ConfigModule } from '@config/config.module';
 import { DatabaseModule } from '../../database/database.module';
 import { UserRepository } from './repository/user.repository';
 import { VolunteerModule } from '../volunteer/volunteer.module';
+import { AssociationModule } from '../association/association.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, forwardRef(() => VolunteerModule)],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    forwardRef(() => VolunteerModule),
+    forwardRef(() => AssociationModule),
+  ],
   controllers: [UserController],
   providers: [UserService, UserRepository],
   exports: [UserService, UserRepository],
