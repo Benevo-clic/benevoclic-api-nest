6da406e1611ce5e8df417ec844de4e35e6c786b1
aboubakar009 <57750832+aboubakardiakite@users.noreply.github.com>
2025-08-18 02:13:45 +0200

    feat: add Dependabot configuration and validation script (#104)

--- src/api/admin/controllers/admin.controller.ts
+++ src/api/admin/controllers/admin.controller.ts
@@ -2,15 +2,18 @@ import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { AdminService } from '../services/admin.service';
 import { AuthGuard } from '../../../guards/auth.guard';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from '../../../common/decorators/roles.decorator';
+import { UserRole } from '../../../common/enums/roles.enum';
 
 @ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @UseGuards(AuthGuard)
-  @ApiBearerAuth()
   @Get(':adminId/check-approval-status')
+  @UseGuards(AuthGuard)
+  @Roles(UserRole.ADMIN)
+  @ApiBearerAuth()
   checkApprovalStatus(@Param('adminId') adminId: string) {
     return this.adminService.checkApprovalStatus(adminId);
   }
